380ce9d43cfe8d9d99139a8feec9ba8c909187f7
janakaudara <janakaudara98@gmail.com>
2023-09-13 09:16:52 +0530

    updated text free range

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
 </head>
 <body>
     <h1> new 1</h1>
-    <p>The purpose of gitignore files is to ensure that certain files not tracked by Git remain untracked. To stop tracking a file that is 
-        currently tracked, use git rm --cached to remove the file from the index. The filename can then be added to the .gitignore file to stop 
+    <p>The janaka udara bandara  is to ensure that certain files not tracked by Git remain untracked. To stop tracking a file that is 
+        currently tracked,  git rm --cached to remove the file from the index. The filename can then be added to the .gitignore file to stop 
         the file from being reintroduced in later commits.</p>
     
 </body>
