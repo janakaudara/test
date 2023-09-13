413e3a180c85f4f5b15db28e05a264fb1d4df931
janakaudara <janakaudara98@gmail.com>
2023-09-13 14:58:24 +0530

    fix bug

---
+++ index1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>new branch</h1>
+    <h1> new 1</h1>
+    <p>The janaka udara bandara  is to ensure that certain files not tracked by Git remain untracked. To stop tracking a file that is 
+        currently tracked,  git rm --cached to remove the file from the index. The filename can then be added to the .gitignore file to stop 
+        the file from being reintroduced in later commits.</p>
+    
+</body>
+</html>
